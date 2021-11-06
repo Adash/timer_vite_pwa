e32f3f67afd8c507c3497a56845efe1c6f9dce2f
Adam K <Adash108@yahoo.com>
2021-11-06 14:37:18 +0000

    icons url

--- vite.config.js
+++ vite.config.js
@@ -13,17 +13,17 @@ export default defineConfig({
       theme_color: '#ffffff',
       icons: [
         {
-          src: './public/logo192x192.png',
+          src: './public/logo192.png',
           sizes: '192x192',
           type: 'image/png',
         },
         {
-          src: './public/logo512x512.png',
+          src: './public/logo512.png',
           sizes: '512x512',
           type: 'image/png',
         },
         {
-          src: './public/logo512x512.png',
+          src: './public/logo512.png',
           sizes: '512x512',
           type: 'image/png',
           purpose: 'any maskable',
